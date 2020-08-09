6ecb964e8f9187cb392cd91acf95cc02e6833643
Romelito Tined <rstined@yahoo.com>
2020-08-09 12:02:53 +0800

    use SQL EXpress - sql login

--- Migrations/20200809031313_search0.cs
+++ Migrations/20200809031313_search0.cs
@@ -2,22 +2,54 @@
 
 namespace SearchFill.Migrations
 {
-    public partial class search2 : Migration
+    public partial class search0 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "SportItems",
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Sport = table.Column<string>(nullable: true),
-                    ItemId = table.Column<int>(nullable: false)
+                    Sport = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_SportItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Person_Country_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Country",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -44,6 +76,11 @@ namespace SearchFill.Migrations
                 name: "IX_Item_SportItemsId",
                 table: "Item",
                 column: "SportItemsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_CountryId",
+                table: "Person",
+                column: "CountryId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -52,7 +89,13 @@ namespace SearchFill.Migrations
                 name: "Item");
 
             migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
                 name: "SportItems");
+
+            migrationBuilder.DropTable(
+                name: "Country");
         }
     }
 }

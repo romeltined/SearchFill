42f8d369966d274edc2c1c0e8465325f8761737d
Romelito Tined <rstined@yahoo.com>
2021-05-01 16:07:50 +0800

    fileupload web API

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SearchFill.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // GET: api/<FilesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<FilesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FilesController>
+        [HttpPost]
+        public async Task<IActionResult> PostFiles(ICollection<IFormFile> files)
+        {
+            var filePath = @"C:\Users\rstin\AppData\Local\Temp\"; // Path.GetTempFileName();
+            var fileName = Path.GetTempFileName();
+            var fileNameTrue="";
+
+            foreach (var formFile in files)
+            {
+                fileNameTrue = formFile.FileName;
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath+fileNameTrue, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                        //formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok();
+
+        }
+
+        private async Task<bool> WriteFile(IFormFile file)
+        {
+            bool isSaveSuccess = false;
+            string fileName;
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                fileName = DateTime.Now.Ticks + extension; //Create a new Name for the file due to security reasons.
+
+                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                   fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                isSaveSuccess = true;
+            }
+            catch (Exception e)
+            {
+                //log error
+            }
+
+            return isSaveSuccess;
+        }
+
+
+        // PUT api/<FilesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FilesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
